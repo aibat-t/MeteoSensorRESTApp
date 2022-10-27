c735a60a244826a3fc40a026b43352f5481d9d52
Aibat <tozhybayev.aibat@gmail.com>
2022-10-27 17:13:52 +0600

    Initial commit

---
+++ src/main/java/kz/aibat/meteoSensorRESTApp/MeteoSensorRestAppApplication.java
@@ -0,0 +1,13 @@
+package kz.aibat.meteoSensorRESTApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoSensorRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteoSensorRestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/aibat/meteoSensorRESTApp/MeteoSensorRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.aibat.meteoSensorRESTApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoSensorRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
